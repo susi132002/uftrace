647afc055df302b6af4ecc26655ccfa09c6746e8
Namhyung Kim <namhyung@gmail.com>
2021-08-15 18:49:13 -0700

    symbol: Allow different path names when loading symbol files
    Symbol files contain pathname of the ELF image and its build-id.  The
    file name is a basename of the image but the pathname can be little
    bit different.  If the build-id matches, we can allow different
    pathname.  This can be useful when stripped binaries are saved in a
    different location.
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

--- utils/symbol.c
+++ utils/symbol.c
@@ -1033,7 +1033,7 @@ static void load_module_symbol(struct symtabs *symtabs, struct uftrace_module *m
 		if (access(symfile, F_OK) == 0) {
 			if (check_symbol_file(symfile, buf, sizeof(buf),
 					      build_id, sizeof(build_id)) > 0 &&
-			    (strcmp(buf, m->name) || strcmp(build_id, m->build_id))) {
+			    (build_id[0] && strcmp(build_id, m->build_id))) {
 				char *new_file;
 
 				new_file = make_new_symbol_filename(symfile,
